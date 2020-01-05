1ce76032b69d4d21a7e5c5560e9d09c2ecfc622d
Tomas Tobar <codetombomb@gmail.com>
2020-01-05 04:35:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,10 +9,9 @@ second_number = 1
 
 
 # test two - subtracting first_number and second_number
-difference = first_number - second_number
+
 
 # Test three - multiply first_number and second_number
-product = first_number * second_number
+
 
 # Test four - Divide first_number and second_number
-quotient = first_number / second_number
